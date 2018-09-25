4e974f31f675a32f399a46e8fb938eddd9d3ed5e
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-09-25 12:13:28 -0500

    Conditionally load data from one of two endpoints depending othe toggled state values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,39 +4,65 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
+        this.seeFilms = this.seeFilms.bind(this);
+        this.seePeople = this.seePeople.bind(this);
 
         this.state = {
             showFilms: false,
+            showPeople: false,
             filmsList: [],
+            peopleList: []
         };
     }
 
     componentDidMount() {
-        fetch("https://ghibliapi.herokuapp.com/films")
+        if (!this.state.showFilms) {
+            fetch("https://ghibliapi.herokuapp.com/films")
             .then(response => response.json())
             .then(films => this.setState({ filmsList: films }));
+        }
+
+        if (!this.state.showPeople) {
+            fetch("https://ghibliapi.herokuapp.com/people")
+            .then(response => response.json())
+            .then(people => this.setState({ peopleList: people }));
+        }
     }
 
-    handleClick() {
+    seeFilms() {
         this.setState({ showFilms: !this.state.showFilms });
     }
 
+    seePeople() {
+        this.setState({ showPeople: !this.state.showPeople });
+    }
+
     render() {
-        if (this.state.showFilms === false) {
+        if (this.state.showPeople) {
             return (
                 <React.Fragment>
-                    <img src={require('../logo.png')} alt='Studio Ghibli logo' />
                     <div>
-                        <button onClick={this.handleClick} className="btn btn-danger mt-5">See films?</button>
+                        <button onClick={this.seePeople} className="btn btn-danger mb-5">Return to home?</button>
                     </div>
+                    {this.state.peopleList.map(person => (
+                        <div key={person.id} className="card text-center">
+                            <div className="card-body">
+                                <h3 className="card-title">{person.name}</h3>
+                                <h5>Age: {person.age}</h5>
+                                <h5>Gender: {person.gender}</h5>
+                                <p className="card-text"><small className="text-muted">Hair color {person.hair_color}</small></p>
+                                <p className="card-text"><small className="text-muted">Eye color: {person.eye_color}</small></p>
+                                <a href={person.url} className="btn btn-danger">See my full info here:</a>
+                            </div>
+                        </div>
+                    ))}
                 </React.Fragment>
             );
-        } else {
+        } else if (this.state.showFilms) {
             return (
                 <React.Fragment>
                     <div>
-                        <button onClick={this.handleClick} className="btn btn-danger mb-5">Return to home?</button>
+                        <button onClick={this.seeFilms} className="btn btn-danger mb-5">Return to home?</button>
                     </div>
                     {this.state.filmsList.map(film => (
                         <div key={film.id} className="card text-center">
@@ -51,6 +77,16 @@ class App extends Component {
                             </div>
                         </div>
                     ))}
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <img src={require('../logo.png')} alt='Studio Ghibli logo' />
+                    <div>
+                        <button onClick={this.seeFilms} className="btn btn-danger mt-5 mr-2">See films?</button>
+                        <button onClick={this.seePeople} className="btn btn-danger mt-5 ml-2">See people?</button>
+                    </div>
                 </React.Fragment>
             );
         }
