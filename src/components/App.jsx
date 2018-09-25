fe095cabff2ca8bcbebcf1f39c9c24308fe85772
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-09-25 11:04:36 -0500

    Completed phase 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,10 @@ class App extends Component {
     constructor(props) {
         super(props);
 
+        this.handleClick = this.handleClick.bind(this);
+
         this.state = {
+            showFilms: false,
             filmsList: [],
         };
     }
@@ -15,24 +18,42 @@ class App extends Component {
             .then(films => this.setState({ filmsList: films }));
     }
 
+    handleClick() {
+        this.setState({ showFilms: !this.state.showFilms });
+    }
+
     render() {
-        return (
-            <React.Fragment>
-                {this.state.filmsList.map(film => (
-                    <div key={film.id} className="card text-center">
-                        <div className="card-body">
-                            <h3 className="card-title">{film.title}</h3>
-                            <h5>Directed by: {film.director}</h5>
-                            <h5>Produced by: {film.producer}</h5>
-                            <p className="card-text">{film.description}</p>
-                            <p className="card-text"><small className="text-muted">Released in {film.release_date}</small></p>
-                            <p className="card-text"><small className="text-muted">Rotten Tomatoes Rating: {film.rt_score}/100</small></p>
-                            <a href={"http://www.google.com/search?q="+ film.title} className="btn btn-danger">Check it out on Google.</a>
-                        </div>
+        if (this.state.showFilms === false) {
+            return (
+                <React.Fragment>
+                    <img src={require('../logo.png')} alt='Studio Ghibli logo' />
+                    <div>
+                        <button onClick={this.handleClick} className="btn btn-danger mt-5">See films?</button>
                     </div>
-                ))}
-            </React.Fragment>
-        );
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <div>
+                        <button onClick={this.handleClick} className="btn btn-danger mb-5">Return to home?</button>
+                    </div>
+                    {this.state.filmsList.map(film => (
+                        <div key={film.id} className="card text-center">
+                            <div className="card-body">
+                                <h3 className="card-title">{film.title}</h3>
+                                <h5>Directed by: {film.director}</h5>
+                                <h5>Produced by: {film.producer}</h5>
+                                <p className="card-text">{film.description}</p>
+                                <p className="card-text"><small className="text-muted">Released in {film.release_date}</small></p>
+                                <p className="card-text"><small className="text-muted">Rotten Tomatoes Rating: {film.rt_score}/100</small></p>
+                                <a href={"http://www.google.com/search?q=" + film.title} className="btn btn-danger">Check it out on Google.</a>
+                            </div>
+                        </div>
+                    ))}
+                </React.Fragment>
+            );
+        }
     }
 }
 
