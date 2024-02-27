d159102855047acce6a59aed44893ad10e35abb1
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-02-27 13:30:08 -0600

    Finally got everything bumped up 6 years

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'isomorphic-fetch';  // Using Chrome 69 but importing for best practices
-import 'es6-promise';      // Using Chrome 69 but importing for best practices
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
